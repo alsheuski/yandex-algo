731a6b7dabfcd7b9b7ce3f8cd702beea299b87b1
Alex Alsheuski <alex@alsheuski.com>
2020-10-17 16:41:30 +0300

    chore(sprint-1): final, add calculator solution

---
+++ sprint-1/final/a-calculator/index.js
@@ -0,0 +1,61 @@
+const fs = require("fs");
+const path = require("path");
+const inputFilePath = path.join(__dirname, "input.txt");
+const outputFilePath = path.join(__dirname, "output.txt");
+const fileLines = fs.readFileSync(inputFilePath, "utf-8").split(/\r?\n/);
+const chars = fileLines[0].split(" ");
+
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  isEmpty() {
+    return !!this.items.length;
+  }
+
+  push(item) {
+    this.items.push(item);
+  }
+
+  pop() {
+    if (this.items.length) {
+      return this.items.pop();
+    } else {
+      return "error";
+    }
+  }
+
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+
+  size() {
+    return this.items.length;
+  }
+}
+
+function solution() {
+  const stack = new Stack();
+
+  for (let i = 0; i < chars.length; i++) {
+    const char = chars[i];
+
+    if (Number.isInteger(+char)) {
+      stack.push(char);
+    } else {
+      if (stack.size() > 1) {
+        const b = stack.pop();
+        const a = stack.pop();
+
+        stack.push(Math.floor(eval(a + char + b)));
+      } else {
+        return -1;
+      }
+    }
+  }
+
+  return stack.peek();
+}
+
+fs.writeFileSync(outputFilePath, solution().toString(), "utf-8");
---
+++ sprint-1/final/a-calculator/test.spec.js
@@ -0,0 +1,67 @@
+const path = require("path");
+const fs = require("fs");
+const vm = require("vm");
+const sinon = require("sinon");
+const solution = fs.readFileSync(path.join(__dirname, "index.js"), "utf-8");
+
+function executeSolution() {
+  vm.runInContext(
+    solution,
+    vm.createContext({ require: require, __dirname: __dirname })
+  );
+}
+
+describe("A. Calculator", () => {
+  const inputFile = path.join(__dirname, "input.txt");
+  const outputFile = path.join(__dirname, "output.txt");
+
+  let fsMock;
+
+  beforeEach(() => {
+    fsMock = sinon.mock(fs);
+  });
+
+  afterEach(() => {
+    fsMock.restore();
+  });
+
+  it(`'3 4 +' should return '7'`, async () => {
+    const input = `3 4 +`;
+    const expected = `7`;
+
+    fsMock.expects("readFileSync").withArgs(inputFile, "utf-8").returns(input);
+    fsMock.expects("writeFileSync").withArgs(outputFile, expected, "utf-8");
+
+    executeSolution();
+  });
+
+  it(`'10 2 4 * -' should return '2'`, async () => {
+    const input = `10 2 4 * -`;
+    const expected = `2`;
+
+    fsMock.expects("readFileSync").withArgs(inputFile, "utf-8").returns(input);
+    fsMock.expects("writeFileSync").withArgs(outputFile, expected, "utf-8");
+
+    executeSolution();
+  });
+
+  it(`'2 1 + 3 *' should return '9'`, async () => {
+    const input = `2 1 + 3 *`;
+    const expected = `9`;
+
+    fsMock.expects("readFileSync").withArgs(inputFile, "utf-8").returns(input);
+    fsMock.expects("writeFileSync").withArgs(outputFile, expected, "utf-8");
+
+    executeSolution();
+  });
+
+  it(`'7 2 + 4 * 2 +' should return '38'`, async () => {
+    const input = `7 2 + 4 * 2 +`;
+    const expected = `38`;
+
+    fsMock.expects("readFileSync").withArgs(inputFile, "utf-8").returns(input);
+    fsMock.expects("writeFileSync").withArgs(outputFile, expected, "utf-8");
+
+    executeSolution();
+  });
+});
